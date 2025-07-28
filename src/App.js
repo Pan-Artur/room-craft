370e06219b0f144b640e8836c1ce96c494b9e072
PanArtur <pankovets.artur1@gmail.com>
2025-07-28 17:06:30 +0300

    creating architecture

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Main } from './pages/Main/Main';
+
+import './App.css';
+
+export const App = () => {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
---
+++ src/pages/Contacts/Contacts.jsx
@@ -0,0 +1,11 @@
+import { Header } from "../../components/Header/Header";
+import { Footer } from "../../components/Footer/Footer";
+
+export const Contacts = () => {
+  return ( 
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
