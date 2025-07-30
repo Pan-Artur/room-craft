dec8a320159e3af58a7a9aae0b61f35e15fd1306
PanArtur <pankovets.artur1@gmail.com>
2025-07-30 15:40:44 +0300

    fixing deploy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header/Header';
 import { Footer } from './components/Footer/Footer';
-
 import { Main } from './pages/Main/Main';
 import { Build } from './pages/Build/Build';
 
-// Для локальної розробки використовуємо '/', для GitHub Pages - '/room-craft/'
-const basename = process.env.NODE_ENV === '/';
+const basename = process.env.PUBLIC_URL || '/';
 
 export const App = () => {
   return (
@@ -14,8 +12,8 @@ export const App = () => {
       <div className="App">
         <Header />
         <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/build" element={<Build />} />
+          <Route index element={<Main />} />
+          <Route path="build" element={<Build />} />
         </Routes>
         <Footer />
       </div>
